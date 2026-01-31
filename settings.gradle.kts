bd5677f53553e39627d3bf45ee7e55c487d9014d
lalatlangau <163626123+lalatlangau@users.noreply.github.com>
2026-01-31 18:40:09 +0800

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+rootProject.name = "Dutamovie"
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+}
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.0.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
+        classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
+    }
+}
